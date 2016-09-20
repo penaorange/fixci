efd95cdaf3d4453bdc52254ea8acb953f8c76b79
gaalih <galih.riyadi14@gmail.com>
2016-09-20 22:46:23 +0700

    disable when fisish examp

--- Peserta_free/views/Tryout_free.php
+++ Peserta_free/views/Tryout_free.php
@@ -13,7 +13,13 @@
               </div>
           </div>
       </div>
-      <?php foreach ($tryout as $key): ?>
+
+      <div class="paginateTryout">
+        <div class="item">
+
+        <?php foreach ($tryout as $key): ?>
+          <div class="konten">
+
                     <div class="col-md-3 tryout animated bounceIn wow delay-03s">
                         <div class="card">
                             <div class="header">
@@ -24,23 +30,42 @@
                               <img src="<?= base_url(); ?>assets/images/sbmptn.jpg" style="width:150px;height:150px;">
                             </div></a>
                             <div class="content">
-                                  <div class="footer text-center">  
+                                  <div class="footer text-center">
                                     <hr>
                                     <form action="<?php echo site_url('tryout-free'); ?>/<?php echo $key->id_tryout; ?>">
                                       <div class="stats">
                                       <button type="submit"class="btn">kerjakan</button>
                                       </div>
-                                    </form>                                    
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
+          </div>
        <?php endforeach; ?>
-                    
+
+      </div>
+
+          <div class="row">
+              <div class="col-md-12">
+                <ul class="pager">
+                  <div class="col-md-12">
+                    <li><a href="#" class="firstPage btn-warning">&laquo; First</a></li>
+                    <li><a href="#" class="previousPage btn-warning">&lsaquo; Previous</a></li>
+                    <li class="pageNumbers"></li>
+                    <li><a href="#" class="nextPage btn-primary">Next &rsaquo;</a></li>
+                    <li><a href="#" class="lastPage btn-primary">End&raquo;</a></li>
+                    <hr/>
+                  </div>
+
+                </ul>
+            </div>
+          </div>
+
+        </div>
+
     </div>
 </div>
 
 <!-- end modular -->
 <?php $this->load->view('PesertaModular/Free/Footer'); ?>
-
-   
